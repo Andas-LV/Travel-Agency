427bacdf3e6f7d9236ee8a4a67ca1dd468104c9d
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-15 22:11:28 +0600

    fetch test.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import styles from "./page.module.css";
+import {useEffect} from "react";
 
 export default function Home() {
+    useEffect(() => {
+        // Здесь используем локальный адрес сервера и конкретный эндпоинт API
+        fetch('http://localhost:8080/api/hello')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message); // Вывод сообщения в консоль
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
   return (
     <main className={styles.main}>
-      hop
+      hoppo
     </main>
   );
 }
