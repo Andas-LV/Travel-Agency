264e2aa75f7cd9d8973be81506a1daae754a1fef
mikobn1 <chisatoplay@gmail.com>
2024-02-15 18:31:38 +0600

    added basic endpoint

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	// Initialize Gin
+	r := gin.Default()
+
+	// Define your routes
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Andas sosi moi huy",
+		})
+	})
+
+	// Run the server
+	r.Run(":8080")
+}
