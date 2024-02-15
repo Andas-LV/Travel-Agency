71340421a994cc9317a713094ad3e405de2dc9e0
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-15 19:53:26 +0600

    prettier.

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import styles from './header.module.css'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            Header<div></div>
+                    <div></div>
+            <div></div><div></div>
+            <div></div>
+                                <div></div>
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
