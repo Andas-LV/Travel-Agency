d0d7811469c17e0e9d7689adb7a1b05ce744e1af
mikobn1 <chisatoplay@gmail.com>
2024-02-16 21:54:08 +0600

    added basic endpoint

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080, ()=>{
+    console.log('Server started on PORT:'+8080)
+  });
 }
 bootstrap();
