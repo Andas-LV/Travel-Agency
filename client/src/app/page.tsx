9713862e28539b84bcdbb9f6cf5f1554f765af56
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-16 12:02:08 +0600

    vercel to server.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,12 @@
-"use client";
-import styles from "./page.module.css";
-import {MainPage} from "@/pages/mainPage/MainPage";
-import {useEffect} from "react";
-export default function Home() {
-    useEffect(() => {
-        fetch('/api/hello', {
-            method: 'GET'
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.message);
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-            });
-    }, []);
+import React from 'react';
+import styles from './page.module.css';
 
+const MainPage = () => {
     return (
-    <main className={styles.main}>
-        i am working 2
-        <MainPage/>
-    </main>
-  );
-}
+        <div>
+            <h1>Welcome to the Main Page!</h1>
+        </div>
+    );
+};
+
+export default MainPage;
--- client/src/pages/mainPage/MainPage.tsx
+++
@@ -1,7 +0,0 @@
-export const MainPage = () => {
-    return(
-        <div>
-            Main page
-        </div>
-    )
-}
