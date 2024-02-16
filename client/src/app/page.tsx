38debbc868db82e8e2ccc9a1208d38df10f179ee
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-16 12:56:28 +0600

    full test 2.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+'use client';
+import React, {useEffect} from 'react';
 import styles from './page.module.css';
 
 const MainPage = () => {
+    useEffect(() => {
+        fetch('/api/hello')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message); // Вывод сообщения в консоль
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
     return (
         <div>
             <h1>Welcome to the Main Page!</h1>
