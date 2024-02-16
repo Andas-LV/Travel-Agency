2c8b16c74ff77e2d6d07b80eda23946365d985b7
mikobn1 <chisatoplay@gmail.com>
2024-02-16 21:51:11 +0600

    changed golang to nest js

--- server/entity/User.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-// User represents a user entity
-type User struct {
-	ID       string `json:"id,omitempty"`
-	Name     string `json:"name,omitempty"`
-	Age      int    `json:"age,omitempty"`
-	Location string `json:"location,omitempty"`
-}
