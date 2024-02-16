e40ca3914cf3b3468eee7a49a5ebf907313e91ba
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-16 11:18:08 +0600

    go.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
 import styles from "./page.module.css";
 import {MainPage} from "@/pages/mainPage/MainPage";
+import {useEffect} from "react";
 export default function Home() {
+    useEffect(() => {
+        fetch('/api/hello', {
+            method: 'GET'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
+
     return (
     <main className={styles.main}>
         i am working 2
