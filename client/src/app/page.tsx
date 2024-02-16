b6c880da1e911556322a13f75b4901cd8ac03b96
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-16 10:54:02 +0600

    builded.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,6 @@
 import styles from "./page.module.css";
-import Route from "@/pages/api/route";
 
 export default function Home() {
-    { Route() }
-
     return (
     <main className={styles.main}>
         i am working
