5d3418fac0cf5eb484efd4b6c7d9c5fa41f91da0
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-02-17 13:01:15 +0600

    full.

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return {message:"Hello API"};
+    return {message:"Hello API hhj"};
   }
 }
