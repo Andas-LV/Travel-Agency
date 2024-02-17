cab30673e97744536f16408a6f19fa4071b13866
mikobn1 <chisatoplay@gmail.com>
2024-02-17 22:51:43 +0600

    connected to db but still can't migrate

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/user';
+import { UserModule } from './modules/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://default:dLK2TmeH7ciq@ep-withered-tooth-a2gw2h8z-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require', // Using the SSL connection string
+      entities:[User],
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // Note: This is necessary if your server uses self-signed certificates; for production, ensure your certificates are properly signed
+        },
+      },
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,20 +1,33 @@
-import { IsEmail, IsInt, IsString, MinLength } from "@nestjs/class-validator";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsEmail, IsInt, IsString, MinLength } from 'class-validator';
 
+@Entity('user')
 export class User {
+    @PrimaryGeneratedColumn()
     @IsInt()
     id: number;
-  
+
+    @Column()
     @IsEmail()
     @MinLength(1)
     email: string;
 
+    @Column()
     @IsString()
-    password:string
+    password: string;
 
-    destinationIds:[]
+    // Assuming destinationIds is an array of integers, adjust based on your schema
+    @Column("int", { array: true })
+    destinationIds: number[];
 
-    boughtDestinationIds:[]
+    // Assuming boughtDestinationIds is an array of integers, adjust based on your schema
+    @Column("int", { array: true })
+    boughtDestinationIds: number[];
 
+    @Column({
+        type: "varchar",
+        default: "USER" // Set the default role value here
+      })
     @IsString()
-    role:string
+    role: string;
 }
