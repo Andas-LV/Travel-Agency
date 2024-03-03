8f22aca423f94e32dbd19a59a564c5734d77cf04
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-03-03 12:00:47 +0500

    500k.

--- client/src/pages/Blog/Blog.tsx
+++ client/src/pages/Blog/Blog.tsx
@@ -18,7 +18,7 @@ export default function Blog() {
                     height={500}
                 />
                 <div className={styles.inform}>
-                    <h1>Beautiful Italy <br/> Let’s travel</h1>
+                    <h1>Beautiful Italy! <br/> Let’s travel</h1>
                     <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system and expound the actual teachings of the great explorer of the truth, the master- builder of human happiness. No one rejects, dislike, or avoids plasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremly painful. Nor again is there anyone who loves or pursues.</p>
                 </div>
             </section>
