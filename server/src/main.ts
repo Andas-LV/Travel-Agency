e06edd3d875f70a35993fcf0ce67bd999d8176d6
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-03-21 16:20:53 +0500

    sigma.

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: ["192.168.45.123", "192.168.45.178", "https://tech-agency-ten.vercel.app","http://localhost:3000"]
-    origin:"*"
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "*",
+    credentials: true,
   });
+
 
   await app.listen(8080, ()=>{
     console.log('Server started on PORT:'+8080)
