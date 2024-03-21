e14a678fb8f4ae505edb752a785f15bad2ccc0b4
Chisato <chisatoplay@gmail.com>
2024-03-22 00:15:11 +0500

    deleted localhost

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = ["http://192.168.45.123:3000", "http://192.168.45.178:3000", "https://tech-agency-ten.vercel.app", "http://localhost:3000"];
+  const allowedOrigins = ["http://192.168.45.123:3000", "http://192.168.45.178:3000", "https://tech-agency-ten.vercel.app"];
 
   app.enableCors({
     origin: function (origin, callback) {
