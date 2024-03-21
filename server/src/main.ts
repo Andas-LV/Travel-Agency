be7f6c796c05a5e94f20660cec8b9c33051387b4
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-03-21 16:40:59 +0500

    sigma.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-import * as cors from 'cors'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cors({
-    origin: "*",
+  const allowedOrigins = ["192.168.45.123", "192.168.45.178", "https://tech-agency-ten.vercel.app", "http://localhost:3000"];
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "*",
     credentials: true,
-  }));
-
+  });
 
   await app.listen(8080, ()=>{
     console.log('Server started on PORT:'+8080)
