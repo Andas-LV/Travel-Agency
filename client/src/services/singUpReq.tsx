bb6ff324aa8f68dda03dc6e65c9b03ed69ed44cb
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-03-24 18:02:57 +0500

    token home.

--- client/src/services/singUpReq.tsx
+++ client/src/services/singUpReq.tsx
@@ -9,6 +9,5 @@ export default function singUpReq({email, password}: SignUpData) {
     axios.post(`${process.env.BACKEND_API_HOST}/user/create`, {email, password})
         .then(res => console.log(res.data))
         .catch(error => console.error(error));
-
     console.log(JSON.stringify({email, password}));
 }
