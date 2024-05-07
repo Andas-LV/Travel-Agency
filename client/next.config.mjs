6da2c71f9a34ced31349cd65707b455877d0b5ad
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-05-07 12:15:28 +0500

    google prod.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
     env: {
         BACKEND_API_HOST: process.env.BACKEND_API_HOST,
+
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+
         GITHUB_SECRET: process.env.GITHUB_SECRET,
         GITHUB_ID: process.env.GITHUB_ID,
     },
