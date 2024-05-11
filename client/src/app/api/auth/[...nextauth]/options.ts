3c2b107b45aa605429c82cdfc85f672b9f742394
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-05-11 22:16:26 +0500

    google prod 4.

--- client/src/app/api/auth/[...nextauth]/options.ts
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -5,12 +5,12 @@ import GitHubProvider from 'next-auth/providers/github'
 export const options: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
         GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
         }),
     ],
 }
