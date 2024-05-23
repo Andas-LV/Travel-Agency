fd5d17b1b9f7dfefd76c317e01c7ad3e7cb89a25
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-05-23 20:34:14 +0500

    autentication prod.

---
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+
+export default NextAuth({
+        providers: [
+            GoogleProvider({
+                clientId: process.env.GOOGLE_CLIENT_ID!,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            }),
+            GitHubProvider({
+                clientId: process.env.GITHUB_ID!,
+                clientSecret: process.env.GITHUB_SECRET!,
+            }),
+        ],
+        secret: process.env.NEXTAUTH_SECRET,
+    }
+)
