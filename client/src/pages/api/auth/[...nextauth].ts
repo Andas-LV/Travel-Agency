59e4c4e7a9b4bd93de5645f9ae77d858c4bf60f8
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-05-24 10:23:10 +0500

    middleware prod.

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
-
 export default NextAuth({
     session: { strategy: "jwt" },
     providers: [
